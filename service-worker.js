014caad236b3b6c7d25f5ad88f03f9421978810f
selas128 <takismasino@gmail.com>
2025-09-26 11:56:57 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "math-quiz-cache-v14";
+const CACHE_NAME = "math-quiz-cache-v15";
 const urlsToCache = [
   "./",
   "./index.html",
@@ -68,6 +68,7 @@ self.addEventListener("fetch", event => {
     caches.match(request).then(response => response || fetch(request))
   );
 });
+
 
 
 
